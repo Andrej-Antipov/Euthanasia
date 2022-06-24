cd651649a4d7848ef0218ab9033528f5c4879642
Andrew <36465597+Andrej-Antipov@users.noreply.github.com>
2022-06-24 23:21:39 +0300

    New logic with debug

--- Euthanasia.sh
+++ Euthanasia.sh
@@ -1,88 +1,46 @@
 #!/bin/bash
 
 # FUNCS
-
-KILL_HAZARDS(){
-if [[ "${gpu}" = "AMD" ]] && [[ "$macos" = "1015" ]]; then 
-    if [[ ! "${danger_applet}" = "no" ]]; then
-        AMDAccel=$( ioreg -c AMDRadeonX4000_AMDAccelDevice  -r | grep IOUserClientCreator )
-        TV_pid=$( echo "${AMDAccel}" | grep TV -m 1 | cut -f2 -d= | cut -c 7- | cut -f1 -d ',' )
-        Safari_pid=$( echo "${AMDAccel}" | grep Safari -m 1 | cut -f2 -d= | cut -c 7- | cut -f1 -d ',' )
-        if [[ ! ${TV_pid} = "" ]]; then echo "${mypassword}" | sudo -Sk osascript -e 'quit app "TV.app"'; fi
-        if [[ "${danger_applet}" = "safari" ]]; then
-            if [[ ! ${Safari_pid} = "" ]]; then echo "${mypassword}" | sudo -Sk osascript -e 'quit app "Safari.app"'; fi
-        fi
-    fi
-fi
-}
-
-GET_POWER_SETTINGS(){
-    display=$( pmset -g | grep displaysleep | awk '{print $NF}' )
-    system=$( pmset -g | tr -d ' ' | grep -ow 'sleep[0-9]*' | sed 's/[^0-9]//g' )
-    if [[ ! ${system} = 0 ]]; then 
-    if [[ ${display} -gt ${system} ]]; then display=${system}; fi
-    let "timer=(system-display)*60"
-    fi
-}
-
-GET_USER_PASSWORD(){
-mypassword="0"
-if (security find-generic-password -a ${USER} -s euthanasia -w) >/dev/null 2>&1; then
-                mypassword=$(security find-generic-password -a ${USER} -s euthanasia -w)
-fi
-}
-
-
-GO_TO_BED(){
-echo "${mypassword}" | sudo -Sk pmset sleepnow
-}
-
-CHECK_DISPLAY(){ 
-pmset -g powerstate | grep -w IODisplayWrangler | xargs | cut -f2 -d' '
-}
-
-SLEEP_TIMER(){
-if [[ ! ${timer} = ${display} ]]; then 
-    touch timer.txt
-    for ((i=0;i<(( ($system-$display)*3));i++))
-    do
-    sleep 20
-    if [[ $( CHECK_DISPLAY ) = 4 ]]; then rm -f timer.txt; break; fi
-    done
-    rm -f timer.txt
-fi
-}
-
+SLEEP_TIMER(){ sleep 30; }
+GO_TO_BED(){ pmset sleepnow; SLEEP_TIMER; }
+CHECK_DISPLAY(){ pmset -g powerstate | grep -w IODisplayWrangler | xargs | cut -f2 -d' ' ; }
+GET_DISPLAY_SLEEP(){ pmset -g | grep displaysleep | awk '{print $2}' ; }
+GET_IDLE_TIMER(){ echo $(ioreg -c IOHIDSystem | grep HIDIdleTime | cut -f2 -d=)/1000000000 | bc ; }
+GET_ASSERTIONS(){  
+	assert=$(pmset -g assertions )
+    echo $(($(echo "${assert}" | grep -m1 PreventUserIdleSystemSleep | awk '{print $NF}')+$(echo "${assert}" | grep -m1 PreventUserIdleDisplaySleep  | awk '{print $NF}')))
+	}
 
 # INIT
 cd "$(dirname "$0")"; ROOT="$(dirname "$0")"
-GET_APP_ICON
-loc=$( cat .SleeperLang.txt)
-if [[ ! $loc = "ru" ]]; then loc="en"; fi 
-GET_USER_PASSWORD
-
-if [[ ! $( system_profiler SPDisplaysDataType | grep Vendor | grep AMD ) = "" ]]; then gpu="AMD"; else gpu="Other"; fi
-
-macos=$( sw_vers -productVersion ); macos=$( echo ${macos//[^0-9]/} ); macos=${macos:0:4}
-
-shikigva=$( sysctl -a | grep -o shikigva=[0-9]* | cut -f2 -d= )
-
-case ${shikigva} in
-16 )   danger_applet="no_safari" ;;
-80 )   danger_applet="safari" ;;
-144 )  danger_applet="no_safari" ;;
-208 )  danger_applet="safari" ;;
-*   )  danger_applet="no"
-esac
-
+echo "init" >> $HOME/Desktop/test.txt
 
 # MAIN
 while true
     do  
+    echo "wait" >> $HOME/Desktop/test.txt
         sleep 20
-        GET_POWER_SETTINGS
-         if [[ ! ${system} = 0 ]]; then        
-            if [[ ! $( CHECK_DISPLAY ) = 4 ]]; then touch timer.txt; KILL_HAZARDS; SLEEP_TIMER; if [[ ! $( CHECK_DISPLAY ) = 4 ]]; then GO_TO_BED; fi; rm -f timer.txt; fi
-        fi
+        if [[ ! $( CHECK_DISPLAY ) = 4 ]]; then 
+				SLEEP_TIMER
+					if [[ ! $( CHECK_DISPLAY ) = 4 ]]; then 
+					echo "sleep" >> $HOME/Desktop/test.txt
+					sleep 3
+					GO_TO_BED
+					fi
+		fi
+				  echo "check timer" >> $HOME/Desktop/test.txt
+				  echo "get display sleep = '"$(($(GET_DISPLAY_SLEEP)*60))"'" >> $HOME/Desktop/test.txt
+				  echo "get idle timer = '"$(GET_IDLE_TIMER)"'" >> $HOME/Desktop/test.txt
+				  if [[ $(($(GET_DISPLAY_SLEEP)*60)) -le $(GET_IDLE_TIMER) ]]; then 
+					echo "check assertions" >> $HOME/Desktop/test.txt
+					echo "assertions = '"$(GET_ASSERTIONS)"'" >> $HOME/Desktop/test.txt
+					if [[ $(GET_ASSERTIONS) = 0 ]]; then 
+						echo "sleep2" >> $HOME/Desktop/test.txt
+						sleep 3
+						GO_TO_BED
+					fi
+				  fi
+				  echo "end pass" >> $HOME/Desktop/test.txt
+
     done
 
